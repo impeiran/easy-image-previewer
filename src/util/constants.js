7c10c3fb459253b964bbbb5d77a69d084b335f18
impeiran <1070045244@qq.com>
2020-02-16 21:10:17 +0800

    develop: add some style sheets

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,8 +1,13 @@
 export const NAMESPACE = 'easy-previewer'
 
+export const CLASS_NAME_CORNER = `${NAMESPACE}__corner`
+export const CLASS_NAME_INDICATOR = `${NAMESPACE}__indicator`
+export const CLASS_NAME_BOARD = `${NAMESPACE}__board`
+export const CLASS_NAME_FOOTER = `${NAMESPACE}__footer`
+
 export const TEMPLATE = `
-  <div class="${NAMESPACE}__corner"></div>
-  <div class="${NAMESPACE}__indicator"></div>
-  <div class="${NAMESPACE}__board"></div>
-  <div class="${NAMESPACE}__footer"></div>
+  <div class="${CLASS_NAME_CORNER}"></div>
+  <div class="${CLASS_NAME_CORNER}"></div>
+  <div class="${CLASS_NAME_BOARD}"></div>
+  <div class="${CLASS_NAME_FOOTER}"></div>
 `
