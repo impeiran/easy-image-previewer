679b406e1b4a7d2e4875a1db0eb389c3ba595d11
impeiran <jiangpeiran1@gosuncn.com>
2020-02-18 17:58:26 +0800

    Build: add production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,59 @@
+const path = require('path')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 module.exports =  {
   mode: 'production',
 
-  entry: './index.js'
+  entry: './index.js',
+  
+  output: {
+    filename: 'easy-image-previewer.min.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'imagePreview',
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    rules: [
+      { 
+        test: /\.js$/ ,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin({
+        cache: true,
+        terserOptions: {
+          ie8: false,
+          keep_fnames: false,
+          output: {
+            beautify: false,
+            comments: false
+          }
+        }
+      }),
+
+      new OptimizeCSSAssetsPlugin()
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'easy-image-previewer.min.css'
+    })
+  ]
 }
